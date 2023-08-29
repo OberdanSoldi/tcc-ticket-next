70e817a12356370846020f12dcb3915e8255c19b
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-08-28 21:35:30 -0300

    adjust httpclient

--- src/domain/httpClient.ts
+++ src/domain/httpClient.ts
@@ -1,11 +1,25 @@
+import { AxiosRequestConfig } from "axios";
+
 export interface HttpClient {
-  get<T>(url: string): Promise<T>;
+  get<T>(url: string, config?: AxiosRequestConfig | undefined): Promise<T>;
 
-  post<T, K>(url: string, body: K): Promise<T>;
+  post<T, K>(
+    url: string,
+    body: K,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T>;
 
-  put<T, K>(url: string, body: K): Promise<T>;
+  put<T, K>(
+    url: string,
+    body: K,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T>;
 
-  delete<T>(url: string): Promise<T>;
+  delete<T>(url: string, config?: AxiosRequestConfig | undefined): Promise<T>;
 
-  patch<T, K>(url: string, body: K): Promise<T>;
+  patch<T, K>(
+    url: string,
+    body: K,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T>;
 }
--- src/helpers/fetch-wrapper.ts
+++ src/helpers/fetch-wrapper.ts
@@ -1,27 +1,49 @@
 "use client";
 
 import { HttpClient } from "@/domain/httpClient";
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 class FetchWrapper implements HttpClient {
-  async get<T>(url: string): Promise<T> {
-    const response: AxiosResponse<T> = await axios.get(url);
+  async get<T>(
+    url: string,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T> {
+    const response: AxiosResponse<T> = await axios.get(url, config);
     return response.data;
   }
-  async post<T, K>(url: string, body: K): Promise<T> {
-    const response: AxiosResponse<T> = await axios.post(url, body);
+
+  async post<T, K>(
+    url: string,
+    body: K,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T> {
+    const response: AxiosResponse<T> = await axios.post(url, body, config);
     return response.data;
   }
-  async put<T, K>(url: string, body: K): Promise<T> {
-    const response: AxiosResponse<T> = await axios.put(url, body);
+
+  async put<T, K>(
+    url: string,
+    body: K,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T> {
+    const response: AxiosResponse<T> = await axios.put(url, body, config);
     return response.data;
   }
-  async delete<T>(url: string): Promise<T> {
-    const response: AxiosResponse<T> = await axios.delete(url);
+
+  async delete<T>(
+    url: string,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T> {
+    const response: AxiosResponse<T> = await axios.delete(url, config);
     return response.data;
   }
-  async patch<T, K>(url: string, body: K): Promise<T> {
-    const response: AxiosResponse<T> = await axios.patch(url, body);
+
+  async patch<T, K>(
+    url: string,
+    body: K,
+    config?: AxiosRequestConfig | undefined
+  ): Promise<T> {
+    const response: AxiosResponse<T> = await axios.patch(url, body, config);
     return response.data;
   }
 }
