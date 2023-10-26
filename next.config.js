211e77c2c510eb883372c0f93a51e5beb8d41c98
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-10-26 07:57:05 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
   },
   env: {
     EXTERNAL_API_URL: process.env.EXTERNAL_API_URL,
-    INTERNAL_API_URL: process.env.INTERNAL_API_URL,
   },
 };
 
--- src/config/clientEnvScheme.ts
+++ src/config/clientEnvScheme.ts
@@ -2,12 +2,10 @@ import z from "zod";
 
 const envSchema = z.object({
   EXTERNAL_API_URL: z.string().url(),
-  INTERNAL_API_URL: z.string().url(),
 });
 
 const clientEnvSchema = envSchema.parse({
-  INTERNAL_API_URL: process.env.INTERNAL_API_URL!,
   EXTERNAL_API_URL: process.env.EXTERNAL_API_URL!,
 });
 
-export const { INTERNAL_API_URL, EXTERNAL_API_URL } = clientEnvSchema;
+export const { EXTERNAL_API_URL } = clientEnvSchema;
