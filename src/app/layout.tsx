7a12fbc4cbe517c5f3fb85dc097827a886af7309
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-11-14 09:23:14 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SDM",
   description: "SDM",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export const dynamic = "force-dynamic";
