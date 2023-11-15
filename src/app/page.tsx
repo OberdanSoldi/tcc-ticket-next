8cb5c234d6b527bc95f074f916b9aa1127e41363
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-11-15 12:41:24 -0300

    fix

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+import { Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import LogoNoText from "@/../public/logo-no-text.svg";
+import CheckIcon from "@mui/icons-material/Check";
+
+import style from "./style.module.scss";
+
+const App = () => {
+  return (
+    <Grid container className={style.container}>
+      <Grid item>
+        <Image src={LogoNoText} width={400} height={350} alt="Logo" />
+      </Grid>
+      <Grid item className={style.titles}>
+        <Typography fontSize={48} variant="h1" color="white">
+          Ticket Manager
+        </Typography>
+        <Typography fontSize={20} variant="h2" color="white">
+          Software de Gerenciamento de Chamados de Manutenção
+        </Typography>
+      </Grid>
+      <Grid item className={style.list}>
+        <Typography variant="subtitle1" color="white">
+          <CheckIcon fontSize="small" /> Segurança
+        </Typography>
+        <Typography variant="subtitle1" color="white">
+          <CheckIcon fontSize="small" /> Organização
+        </Typography>
+        <Typography variant="subtitle1" color="white">
+          <CheckIcon fontSize="small" /> Facilidade de Uso
+        </Typography>
+        <Typography variant="subtitle1" color="white">
+          <CheckIcon fontSize="small" /> Permissões Personalizadas
+        </Typography>
+        <Typography variant="subtitle1" color="white">
+          <CheckIcon fontSize="small" /> Eficiência no gerenciamento
+        </Typography>
+        <Typography variant="subtitle1" color="white">
+          <CheckIcon fontSize="small" /> Acesso restrito para convidados
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
